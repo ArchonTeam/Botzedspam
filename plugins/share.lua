4a22340085adfb2d019d2cea2851253e6d523223
Archon Team <rezafardipour2@gmail.com>
2016-04-15 17:23:09 +0430

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)     -- Bot number     -- first name        -- last name
-send_contact(get_receiver(msg), "+639080453961", "TeleManager Plus", "@telemanager_ch", ok_cb, false)
+send_contact(get_receiver(msg), "098900000000", "Nobotnumber", "Nobot number", ok_cb, false)
 end
 
 return {
